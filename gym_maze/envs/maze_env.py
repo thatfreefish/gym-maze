aa50712c87c9bd3704296f93b1cff859754ec509
thatfreefish <33439820+thatfreefish@users.noreply.github.com>
2018-05-01 08:38:25 +0800

    Update maze_env.py

--- gym_maze/envs/maze_env.py
+++ gym_maze/envs/maze_env.py
@@ -3,7 +3,7 @@ import numpy as np
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
-from gym_maze.envs.maze_view_2d import MazeView2D
+from gym_maze.envs.maze_view_2d import MazeView2D #envs.maze_view_2d.py文件中的class：MazeView2D
 
 
 class MazeEnv(gym.Env):
